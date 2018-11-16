0192212828472580e70541528d3312d5537dc459
Jitendra Singh <jbisht@eagleinvsys.com>
2018-11-16 16:27:00 +0530

    pagination logic added

--- graphql-db-example/src/main/java/demo/graphql/services/AllUsersFetcher.java
+++ graphql-db-example/src/main/java/demo/graphql/services/AllUsersFetcher.java
@@ -5,6 +5,7 @@ import demo.graphql.common.repositories.UserRepository;
 import graphql.schema.DataFetcher;
 import graphql.schema.DataFetchingEnvironment;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -18,6 +19,8 @@ public class AllUsersFetcher implements DataFetcher<List<User>>
     @Override
     public List<User> get(DataFetchingEnvironment environment)
     {
-        return userRepository.findAll();
+        int skip = environment.getArgument("skip");
+        int limit = environment.getArgument("first");
+        return userRepository.findAll(PageRequest.of(skip, limit)).getContent();
     }
 }
